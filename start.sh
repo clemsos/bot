494662e9132520b5169f74773c79ffc2c0aca556
clemsos <clement.renaud@gmail.com>
2014-06-27 15:31:23 +0200

    add scripts

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 export HUBOT_IRC_ROOMS='#ls58'
 export HUBOT_IRC_SERVER="irc.freenode.org"
-bin/hubot -a irc -n bot
+export HUBOT_IRC_NICK="jeanlouis"
+export HUBOT_IRC_UNFLOOD="true"
+
+bin/hubot -a irc -n jeanlouis
